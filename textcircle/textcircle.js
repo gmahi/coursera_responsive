7a7080bd5cdf3725e2b1d906b3d9e1a390e5c34d
gmahi <gullamahi@gmail.com>
2015-12-19 15:55:33 +0100

    timer

---
+++ textcircle/textcircle.js
@@ -0,0 +1,41 @@
+this.Documents = new Mongo.Collection('documents');
+
+if (Meteor.isClient) {
+
+    //update the session current_date 
+    //variable every 1000 ms
+    Meteor.setInterval(function () {
+        Session.set("current_date", new Date());
+    }, 1000);
+
+    Template.date_display.helpers({
+        "current_date": function () {
+            return Session.get("current_date");
+        }
+    })
+
+
+    Template.editor.helpers({
+        docid: function () {
+            var doc = Documents.findOne();
+            if (doc) {
+                return doc._id;
+            }
+            else {
+                return undefined;
+            }
+           
+        }
+    });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+      // code to run on server at startup
+
+      if (!Documents.findOne()) {// no documents yet!
+          Documents.insert({ title: "my new Document" });
+
+      }
+  });
+}
