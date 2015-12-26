6297fdaf9713a90e936d9151dd69fb37a6dd48a1
gmahi <gullamahi@gmail.com>
2015-12-26 11:09:33 +0100

    end of the 1st week

--- textcircle/textcircle.js
+++ textcircle/textcircle.js
@@ -2,17 +2,7 @@ this.Documents = new Mongo.Collection('documents');
 
 if (Meteor.isClient) {
 
-    //update the session current_date 
-    //variable every 1000 ms
-    Meteor.setInterval(function () {
-        Session.set("current_date", new Date());
-    }, 1000);
-
-    Template.date_display.helpers({
-        "current_date": function () {
-            return Session.get("current_date");
-        }
-    })
+   
 
 
     Template.editor.helpers({
@@ -25,7 +15,16 @@ if (Meteor.isClient) {
                 return undefined;
             }
            
-        }
+        },
+        config: function () {
+            return function (editor) {
+                editor.on("change", function (cm_editor, info) {
+                    console.log(cm_editor.getValue());
+                    $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+                });
+             
+            }
+        },
     });
 }
 
